5f3b5b8a6939d4e2f5842d790759d4b89a28cd8a
BlackSound1 <matthewr@videotron.ca>
2025-12-20 22:54:33 -0500

    Initial work

---
+++ components/NavBar.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NavBar() {
+    return (
+        <header>
+            <nav>
+                <Link className="logo" href={"/"} >
+                    <Image
+                        src={"/icons/logo.png"}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                    />
+
+                    <p>DevEvent</p>
+                </Link>
+
+                <ul>
+                    <Link href={"/"}>Home</Link>
+                    <Link href={"/"}>Events</Link>
+                    <Link href={"/"}>Create Event</Link>
+                </ul>
+            </nav>
+        </header>
+    );
+}
